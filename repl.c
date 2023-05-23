06d283911c2c956eee6562b040eed609cd221504
itscomputers <its_computers@protonmail.com>
2023-05-23 08:53:58 -0600

    add error handling

--- repl.c
+++ repl.c
@@ -7,6 +7,49 @@
 #include <editline/readline.h>
 #include "mpc.h"
 
+enum { RESULT_NUM, RESULT_ERR };
+enum { ERR_DIV_ZERO, ERR_BAD_OP, ERR_BAD_NUM, ERR_UNKNOWN };
+
+typedef struct {
+  int type;
+  long value;
+  int error;
+} Result;
+
+Result result_num(long v) {
+  Result r = {.type=RESULT_NUM, .value=v};
+  return r;
+}
+
+Result result_err(int e) {
+  Result r = {.type=RESULT_ERR, .error=e};
+  return r;
+}
+
+char* result_err_msg(Result r) {
+  switch (r.error) {
+    case ERR_DIV_ZERO:
+      return "error: division by zero";
+    case ERR_BAD_OP:
+      return "error: unknown operator";
+    case ERR_BAD_NUM:
+      return "error: unable to parse number";
+  }
+  return "unknown error";
+}
+
+void result_print(Result r) {
+  switch (r.type) {
+    case RESULT_NUM:
+      printf("~>  %li", r.value);
+      break;
+    case RESULT_ERR:
+      printf("%s", result_err_msg(r));
+      break;
+  }
+  putchar('\n');
+}
+
 void startup_info(void) {
   puts("its-lisp v0.1\nctrl-c to exit\n");
 }
@@ -17,21 +60,31 @@ int should_exit(char* input) {
   return exit == 0 || quit == 0;
 }
 
-long eval_op(char* op, int a, int b) {
-  if (strcmp(op, "+") == 0) { return a + b; }
-  if (strcmp(op, "-") == 0) { return a - b; }
-  if (strcmp(op, "*") == 0) { return a * b; }
-  if (strcmp(op, "/") == 0) { return a / b; }
-  return 0;
+Result eval_op(char* op, Result a, Result b) {
+  if (a.type == RESULT_ERR) { return a; }
+  if (b.type == RESULT_ERR) { return b; }
+  if (strcmp(op, "+") == 0) { return result_num(a.value + b.value); }
+  if (strcmp(op, "-") == 0) { return result_num(a.value - b.value); }
+  if (strcmp(op, "*") == 0) { return result_num(a.value * b.value); }
+  if (strcmp(op, "/") == 0) {
+    return b.value == 0 ?
+      result_err(ERR_DIV_ZERO) :
+      result_num(a.value / b.value);
+  }
+  return result_err(ERR_BAD_OP);
 }
 
-long eval(mpc_ast_t* t) {
+Result eval(mpc_ast_t* t) {
   if (strstr(t->tag, "number")) {
-    return atoi(t->contents);
+    errno = 0;
+    long value = strtol(t->contents, NULL, 10);
+    return errno == ERANGE ?
+      result_err(ERR_BAD_NUM) :
+      result_num(value);
   }
 
   char* op = t->children[1]->contents;
-  long result = eval(t->children[2]);
+  Result result = eval(t->children[2]);
 
   int i = 3;
   while (strstr(t->children[i]->tag, "expr")) {
@@ -45,7 +98,7 @@ long eval(mpc_ast_t* t) {
 void process_input(char* input, mpc_parser_t* lisp) {
   mpc_result_t r;
   if (mpc_parse("<stdin>", input, lisp, &r)) {
-    printf("~>  %li\n", eval(r.output));
+    result_print(eval(r.output));
     mpc_ast_delete(r.output);
   } else {
     mpc_err_print(r.error);
