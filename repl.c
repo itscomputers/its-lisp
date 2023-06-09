acb72bf83586d2d7251e699c01e722e6eac38ba9
itscomputers <its_computers@protonmail.com>
2023-06-09 13:09:27 -0600

    refactor to use function types

--- repl.c
+++ repl.c
@@ -13,6 +13,7 @@ enum {
   VAL_NUM,
   VAL_ERR,
   VAL_SYM,
+  VAL_FUN,
   VAL_SEXPR,
   VAL_QEXPR
 };
@@ -21,6 +22,7 @@ enum {
   ERR_DIV_ZERO,
   ERR_BAD_FUNCTION,
   ERR_BAD_NUM,
+  ERR_BAD_SYM,
   ERR_BAD_SEXPR,
   ERR_NON_NUM_ARG,
   ERR_WRONG_NUM_ARGS,
@@ -29,19 +31,37 @@ enum {
   ERR_UNKNOWN
 };
 
-typedef struct Val {
+struct Val;
+typedef struct Val Val;
+
+struct Env;
+typedef struct Env Env;
+
+typedef Val*(*BuiltIn)(Env*, Val*);
+
+struct Val {
   int type;
+
   long num;
   char* sym;
   char* err;
+  BuiltIn fun;
+
   int count;
-  struct Val** cell;
-} Val;
+  Val** cell;
+};
+
+struct Env {
+  int count;
+  char** syms;
+  Val** vals;
+};
 
 Val* val_read(mpc_ast_t* t);
-Val* val_eval(Val* v);
+Val* val_eval(Env* e, Val* v);
 void val_print(Val* v);
 void val_println(Val* v);
+Val* env_get(Env* e, Val* k);
 
 /*
  * Val builders
@@ -67,6 +87,9 @@ Val* val_err(int e) {
       break;
     case ERR_BAD_NUM:
       msg = "unable to parse number";
+      break;
+    case ERR_BAD_SYM:
+      msg = "unbound symbol";
       break;
     case ERR_BAD_SEXPR:
       msg = "s-expression does not start with symbol";
@@ -99,6 +122,13 @@ Val* val_sym(char* s) {
   return v;
 }
 
+Val* val_fun(BuiltIn func) {
+  Val* v = malloc(sizeof(Val));
+  v->type = VAL_FUN;
+  v->fun = func;
+  return v;
+}
+
 Val* val_sexpr(void) {
   Val* v = malloc(sizeof(Val));
   v->type = VAL_SEXPR;
@@ -121,12 +151,14 @@ Val* val_qexpr(void) {
 
 void val_del(Val* v) {
   switch (v->type) {
-    case VAL_NUM: break;
+    case VAL_NUM:
+    case VAL_FUN:
+      break;
     case VAL_ERR: free(v->err); break;
     case VAL_SYM: free(v->sym); break;
     case VAL_SEXPR:
     case VAL_QEXPR:
-      for (int i=0; i < v->count; i++) {
+      for (int i = 0; i < v->count; i++) {
         val_del(v->cell[i]);
       }
       free(v->cell);
@@ -134,6 +166,34 @@ void val_del(Val* v) {
   }
 
   free(v);
+}
+
+Val* val_copy(Val* v) {
+  Val* c = malloc(sizeof(Val));
+  c->type = v->type;
+
+  switch (v->type) {
+    case VAL_NUM: c->num = v->num; break;
+    case VAL_FUN: c->fun = v->fun; break;
+    case VAL_ERR:
+      c->err = malloc(strlen(v->err) + 1);
+      strcpy(c->err, v->err);
+      break;
+    case VAL_SYM:
+      c->sym = malloc(strlen(v->sym) + 1);
+      strcpy(c->sym, v->sym);
+      break;
+    case VAL_SEXPR:
+    case VAL_QEXPR:
+      c->count = v->count;
+      c->cell = malloc(sizeof(Val*) * c->count);
+      for (int i = 0; i < c->count; i++) {
+        c->cell[i] = val_copy(v->cell[i]);
+      }
+    break;
+  }
+
+  return c;
 }
 
 Val* val_append(Val* v, Val* c) {
@@ -169,7 +229,7 @@ Val* val_join(Val* a, Val* b) {
  * Val evaluators
  */
 
-Val* builtin_head(Val* args) {
+Val* builtin_head(Env* e, Val* args) {
   ASSERT(args, args->count == 1, ERR_WRONG_NUM_ARGS);
   ASSERT(args, args->cell[0]->type == VAL_QEXPR, ERR_WRONG_ARG_TYPE);
   ASSERT(args, args->cell[0]->count != 0, ERR_EMPTY_ARGS);
@@ -180,7 +240,7 @@ Val* builtin_head(Val* args) {
   return v;
 }
 
-Val* builtin_tail(Val* args) {
+Val* builtin_tail(Env* e, Val* args) {
   ASSERT(args, args->count == 1, ERR_WRONG_NUM_ARGS);
   ASSERT(args, args->cell[0]->type == VAL_QEXPR, ERR_WRONG_ARG_TYPE);
   ASSERT(args, args->cell[0]->count != 0, ERR_EMPTY_ARGS);
@@ -189,20 +249,20 @@ Val* builtin_tail(Val* args) {
   return v;
 }
 
-Val* builtin_list(Val* args) {
+Val* builtin_list(Env* e, Val* args) {
   args->type = VAL_QEXPR;
   return args;
 }
 
-Val* builtin_eval(Val* args) {
+Val* builtin_eval(Env* e, Val* args) {
   ASSERT(args, args->count == 1, ERR_WRONG_NUM_ARGS);
   ASSERT(args, args->cell[0]->type == VAL_QEXPR, ERR_WRONG_ARG_TYPE);
   Val* v = val_take(args, 0);
   v->type = VAL_SEXPR;
-  return val_eval(v);
+  return val_eval(e, v);
 }
 
-Val* builtin_join(Val* args) {
+Val* builtin_join(Env* e, Val* args) {
   for (int i = 0; i < args->count; i++) {
     ASSERT(args, args->cell[i]->type == VAL_QEXPR, ERR_WRONG_ARG_TYPE);
   }
@@ -214,7 +274,37 @@ Val* builtin_join(Val* args) {
   return v;
 }
 
-Val* builtin_op(Val* v, char* op) {
+Val* builtin_op(Env* e, Val* v, char* op);
+
+Val* builtin_add(Env* e, Val* v) {
+  return builtin_op(e, v, "+");
+}
+
+Val* builtin_sub(Env* e, Val* v) {
+  return builtin_op(e, v, "-");
+}
+
+Val* builtin_mul(Env* e, Val* v) {
+  return builtin_op(e, v, "*");
+}
+
+Val* builtin_div(Env* e, Val* v) {
+  return builtin_op(e, v, "/");
+}
+
+Val* builtin_mod(Env* e, Val* v) {
+  return builtin_op(e, v, "%");
+}
+
+Val* builtin_min(Env* e, Val* v) {
+  return builtin_op(e, v, "min");
+}
+
+Val* builtin_max(Env* e, Val* v) {
+  return builtin_op(e, v, "max");
+}
+
+Val* builtin_op(Env* e, Val* v, char* op) {
   for (int i=0; i < v->count; i++) {
     if (v->cell[i]->type != VAL_NUM) {
       val_del(v);
@@ -257,22 +347,11 @@ Val* builtin_op(Val* v, char* op) {
   return a;
 }
 
-Val* builtin(Val* v, char* func) {
-  if (strcmp(func, "head") == 0) { return builtin_head(v); }
-  if (strcmp(func, "tail") == 0) { return builtin_tail(v); }
-  if (strcmp(func, "list") == 0) { return builtin_list(v); }
-  if (strcmp(func, "eval") == 0) { return builtin_eval(v); }
-  if (strcmp(func, "join") == 0) { return builtin_join(v); }
-  if (strstr("+-*/%minmax", func)) { return builtin_op(v, func); }
-  val_del(v);
-  return val_err(ERR_BAD_FUNCTION);
-}
-
-Val* val_eval_sexpr(Val* v) {
+Val* val_eval_sexpr(Env* e, Val* v) {
   if (v->count == 0) { return v; }
 
   for (int i=0; i < v->count; i++) {
-    v->cell[i] = val_eval(v->cell[i]);
+    v->cell[i] = val_eval(e, v->cell[i]);
   }
 
   if (v->count == 1) { return val_take(v, 0); }
@@ -281,21 +360,27 @@ Val* val_eval_sexpr(Val* v) {
     if (v->cell[i]->type == VAL_ERR) { return val_take(v, i); }
   }
 
-  Val* vs = val_pop(v, 0);
-  if (vs->type != VAL_SYM) {
-    val_del(vs);
+  Val* f = val_pop(v, 0);
+  if (f->type != VAL_FUN) {
+    val_del(f);
     val_del(v);
-    return val_err(ERR_BAD_SEXPR);
+    return val_err(ERR_BAD_FUNCTION);
   }
 
-  Val* r = builtin(v, vs->sym);
-  val_del(vs);
+  Val* r = f->fun(e, v);
+  val_del(f);
 
   return r;
 }
 
-Val* val_eval(Val* v) {
-  if (v->type == VAL_SEXPR) { return val_eval_sexpr(v); }
+Val* val_eval(Env* e, Val* v) {
+  // todo: refactor to use switch
+  if (v->type == VAL_SYM) {
+    Val* x = env_get(e, v);
+    val_del(v);
+    return x;
+  }
+  if (v->type == VAL_SEXPR) { return val_eval_sexpr(e, v); }
   return v;
 }
 
@@ -358,6 +443,9 @@ void val_print(Val* v) {
     case VAL_SYM:
       printf("%s", v->sym);
       break;
+    case VAL_FUN:
+      printf("<function>");
+      break;
     case VAL_SEXPR:
       val_expr_print(v, '(', ')');
       break;
@@ -376,6 +464,85 @@ void val_println(Val* v) {
 }
 
 /*
+ * Env operators
+ */
+
+Env* env_new(void) {
+  Env* e = malloc(sizeof(Env));
+  e->count = 0;
+  e->syms = NULL;
+  e->vals = NULL;
+  return e;
+}
+
+void env_del(Env* e) {
+  for (int i = 0; i < e->count; i++) {
+    free(e->syms[i]);
+    val_del(e->vals[i]);
+  }
+  free(e->syms);
+  free(e->vals);
+  free(e);
+}
+
+Val* env_get(Env* e, Val* k) {
+  for (int i = 0; i < e->count; i++) {
+    if (strcmp(e->syms[i], k->sym) == 0) {
+      return val_copy(e->vals[i]);
+    }
+  }
+  return val_err(ERR_BAD_SYM);
+}
+
+void env_put(Env* e, Val* k, Val* v) {
+  for (int i = 0; i < e->count; i++) {
+    if (strcmp(e->syms[i], k->sym) == 0) {
+      val_del(e->vals[i]);
+      e->vals[i] = val_copy(v);
+      return;
+    }
+  }
+  e->count++;
+
+  e->vals = realloc(e->vals, sizeof(Val*) * e->count);
+  e->vals[e->count - 1] = val_copy(v);
+
+  e->syms = realloc(e->syms, sizeof(char*) * e->count);
+  e->syms[e->count - 1] = malloc(strlen(k->sym) + 1);
+  strcpy(e->syms[e->count - 1], k->sym);
+}
+
+void env_add_builtin(Env* e, char* name, BuiltIn func) {
+  Val* k = val_sym(name);
+  Val* v = val_fun(func);
+  env_put(e, k, v);
+  val_del(k);
+  val_del(v);
+}
+
+void env_add_builtins(Env* e) {
+  env_add_builtin(e, "+", builtin_add);
+  env_add_builtin(e, "-", builtin_sub);
+  env_add_builtin(e, "*", builtin_mul);
+  env_add_builtin(e, "/", builtin_div);
+  env_add_builtin(e, "%", builtin_mod);
+  env_add_builtin(e, "min", builtin_min);
+  env_add_builtin(e, "max", builtin_max);
+
+  env_add_builtin(e, "list", builtin_list);
+  env_add_builtin(e, "head", builtin_head);
+  env_add_builtin(e, "tail", builtin_tail);
+  env_add_builtin(e, "eval", builtin_eval);
+  env_add_builtin(e, "join", builtin_join);
+}
+
+Env* env_init(void) {
+  Env* e = env_new();
+  env_add_builtins(e);
+  return e;
+}
+
+/*
  * Control functions
  */
 
@@ -389,11 +556,11 @@ int should_exit(char* input) {
   return exit == 0 || quit == 0;
 }
 
-void process_input(char* input, mpc_parser_t* lisp) {
+void process_input(Env* e, char* input, mpc_parser_t* lisp) {
   mpc_result_t r;
   if (mpc_parse("<stdin>", input, lisp, &r)) {
     // mpc_ast_print(r.output);
-    Val* v = val_eval(val_read(r.output));
+    Val* v = val_eval(e, val_read(r.output));
     val_println(v);
     val_del(v);
   } else {
@@ -414,10 +581,7 @@ int main(int argc, char** argv) {
     MPCA_LANG_DEFAULT,
     "                                                         \
       number    : /-?[0-9]+/ ;                                \
-      symbol    : '+' | '-' | '*' | '/' | '%'                 \
-                | \"min\" | \"max\" | \"list\" | \"head\"     \
-                | \"tail\" | \"join\" | \"eval\" | \"cons\"   \
-                | \"len\" ;                                   \
+      symbol    : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/ ;          \
       sexpr     : '(' <expr>* ')' ;                           \
       qexpr     : '{' <expr>* '}' ;                           \
       expr      : <number> | <symbol> | <sexpr> | <qexpr> ;   \
@@ -433,12 +597,16 @@ int main(int argc, char** argv) {
 
   startup_info();
 
+  Env* env = env_init();
+
   while (1) {
     char* input = readline("its> ");
     if (should_exit(input)) break;
     add_history(input);
-    process_input(input, ItsLisp);
+    process_input(env, input, ItsLisp);
   }
+
+  env_del(env);
 
   mpc_cleanup(6,
     Number,
